198bb2124ede55803ff75944a7f1839bfb7b0e75
glaubernespoli <glauber.nespoli@gmail.com>
2020-08-04 12:10:43 +0200

    server routes

--- 01-HTML/02-ADVANCED/app.js
+++ 01-HTML/02-ADVANCED/app.js
@@ -6,18 +6,30 @@ const app = express();
 //
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended:false
+    extended: false
 }));
 
 //define routes
-const server = app.listen(5000, function(){
+const server = app.listen(5000, function () {
     console.log('Node server is running...');
 });
 
-app.get('/', function(req, res){
-	res.sendFile('/home/glaubernespoli/workspace/dci/FbW39_Lessons/01-HTML/02-ADVANCED/index.html');
+app.get('/', function (req, res) {
+    res.sendFile('/home/glaubernespoli/workspace/dci/FbW39_Lessons/01-HTML/02-ADVANCED/index.html');
 });
 
-app.post('/', function(req, res){
-    res.send('<h1>'+req.body.name+'</h1>');
+app.post('/submit', function (req, res) {
+    res.send(`<div>
+	<h1>Welcome ${req.body.name}!</h1>
+	<ul>
+		<li><b>Name:</b> ${req.body.name}</li>
+		<li><b>Email:</b> ${req.body.email}</li>
+		<li><b>Message:</b> ${req.body.message}</li>
+		<li><b>Gender:</b> ${req.body.gender}</li>
+		<li><b>Age:</b> ${req.body.age}</li>
+		<li><b>Birthdate:</b> ${req.body.birthdate}</li>
+		<li><b>Membership:</b> ${req.body.membership}</li>
+		<li><b>Transports I like: </b>${req.body.transport}</li>
+	</ul>
+</div>`);
 });
