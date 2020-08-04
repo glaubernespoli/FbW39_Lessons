c7bfce8ac7d6832ac7350b8bb3e2d0edee91927c
glaubernespoli <glauber.nespoli@gmail.com>
2020-08-04 22:05:59 +0200

    errr

--- 01-HTML/02-ADVANCED/app.js
+++ 01-HTML/02-ADVANCED/app.js
@@ -6,24 +6,24 @@ const app = express();
 //
 app.use(bodyParser.json());
 app.use(
-	bodyParser.urlencoded({
-		extended: false,
-	})
+    bodyParser.urlencoded({
+        extended: false,
+    })
 );
 
 //define routes
 const server = app.listen(5000, function () {
-	console.log("Node server is running...");
+    console.log("Node server is running...");
 });
 
 app.get("/", function (req, res) {
-	res.sendFile(
-		"/home/glaubernespoli/workspace/dci/FbW39_Lessons/01-HTML/02-ADVANCED/index.html"
-	);
+    res.sendFile(
+        "/home/glaubernespoli/workspace/dci/FbW39_Lessons/01-HTML/02-ADVANCED/lalala.html"
+    );
 });
 
 app.post("/submit", function (req, res) {
-	res.send(`<div>
+    res.send(`<div>
 	<h1>Welcome ${req.body.name}!</h1>
 	<ul>
 		<li><b>Name:</b> ${req.body.name}</li>
