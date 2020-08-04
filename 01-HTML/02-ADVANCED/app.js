46f97ce255eda6c60f60284b64c5cceaf13a479b
glaubernespoli <glauber.nespoli@gmail.com>
2020-08-04 21:48:38 +0200

    another stupid change

--- 01-HTML/02-ADVANCED/app.js
+++ 01-HTML/02-ADVANCED/app.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 //
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false,
+	})
+);
 
 //define routes
 const server = app.listen(5000, function () {
-    console.log('Node server is running...');
+	console.log("Node server is running...");
 });
 
-app.get('/', function (req, res) {
-    res.sendFile('/home/glaubernespoli/workspace/dci/FbW39_Lessons/01-HTML/02-ADVANCED/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(
+		"/home/glaubernespoli/workspace/dci/FbW39_Lessons/01-HTML/02-ADVANCED/about.html"
+	);
 });
 
-app.post('/submit', function (req, res) {
-    res.send(`<div>
+app.post("/submit", function (req, res) {
+	res.send(`<div>
 	<h1>Welcome ${req.body.name}!</h1>
 	<ul>
 		<li><b>Name:</b> ${req.body.name}</li>
