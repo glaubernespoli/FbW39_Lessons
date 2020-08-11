375e5026813e306ca55859826dbbde4c009391c9
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2020-08-11 12:47:45 +0200

    12:45 11 Aug 2020

--- 02-CSS/03_Position/server.js
+++ 02-CSS/03_Position/server.js
@@ -1,14 +1,11 @@
-const express = require("express");
+const express = require('express');
 const server = express();
 
-//serve a static files
-server.use(
-    express.static(
-        "/home/safwan/Documents/fbw39/FbW39_Lessons/02-CSS/03_Position", {
-            index: "index.html",
-        }
-    )
-);
+//serve static files
+server.use(express.static("/home/safwan/Documents/fbw39/live-code/03_Position"));
 
-const port = 2022;
-server.listen(port);
+
+const port = 2024;
+server.listen(port, function () {
+    console.log(`Server is listning to port ${port}`);
+});
