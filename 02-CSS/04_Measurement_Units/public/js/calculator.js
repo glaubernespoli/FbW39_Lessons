dc74711a591929822f0f4c96c6dda3afffba7ec8
glaubernespoli <glauber.nespoli@gmail.com>
2020-08-18 14:18:39 +0200

    fadein and out inputs

--- 02-CSS/04_Measurement_Units/public/js/calculator.js
+++ 02-CSS/04_Measurement_Units/public/js/calculator.js
@@ -7,8 +7,18 @@ import {
  * @param {number} v the desired vw
  */
 function vw(v) {
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var w = Math.max(
+        document.documentElement.clientWidth,
+        window.innerWidth || 0
+    );
     return (v * w) / 100;
+}
+
+//TODO pegar width do parent?
+function getMaxValueRangeFor(unitLabel) {
+    const unit = units[unitLabel];
+    const maxWidth = vw(88);
+    return unitLabel === 'px' ? maxWidth : maxWidth / unit.conversion.px;
 }
 
 /**
@@ -27,9 +37,7 @@ function setCurrentActiveUnit(button) {
  * Updates the unit-info container with the new information.
  * @param {object} button the button element
  */
-function updateInfoDisplay(button) {
-    const currentUnitLabel = $(button).data('unit');
-    console.log($('.unit-info-wrapper'));
+function updateInfoDisplay(currentUnitLabel) {
     $('.unit-info-wrapper').replaceWith(buildInfoDisplayFor(currentUnitLabel));
 }
 
@@ -57,36 +65,60 @@ function buildInfoDisplayFor(currentUnitLabel) {
  * @param {object} button the button element
  */
 function updateSelectedContent(button) {
-    $('.selected-wrapper').fadeOut(300, () => {
-        updateInfoDisplay(button);
-    }).fadeIn(300);
+    const currentUnitLabel = $(button).data('unit');
+    $('.selected-wrapper')
+        .fadeOut(300, () => {
+            updateInfoDisplay(currentUnitLabel);
+            updateRangeValues(currentUnitLabel);
+        })
+        .fadeIn(300);
 }
 
+//TODO
+function updateRangeValues(currentUnitLabel) {
+    const maxWidth = getMaxValueRangeFor(currentUnitLabel);
+
+    $('#unit-range-min').val(0);
+    $('#unit-range-current').val(Math.round(maxWidth / 2));
+    $('#unit-range-max').val(Math.round(maxWidth));
+}
 /**
  * Actions to be done once the unit button is clicked.
  */
-$('div.unit-wrapper button').click(e => {
+$('div.unit-wrapper button').click((e) => {
     const button = e.target;
     setCurrentActiveUnit(button);
     updateSelectedContent(button);
 });
 
 /**
- * Actions to be done once one of the unit range buttons is clicked.
+ * Initially setting the click event to the range box.
  */
-$('div.unit-range-box').click(e => {
-    const rangeElement = e.target;
-    console.log(rangeElement);
-    $(rangeElement).children('span').hide();
-    $(rangeElement).children('input').fadeIn(300).focus();
-});
+$('div.unit-range-box').click(e => rangeBoxClickEvent(e.target));
 
 /**
- * Actions to be done once one of the unit range inputs loses focus.
+ * initially setting the focusout and keyup event to the range inputs
  */
-$('div.unit-range-box input').focusout(e => {
-    const inputElement = e.target;
-    const parentDiv = $(inputElement).parent();
-    $(inputElement).hide();
+$('div.unit-range-box input').on('focusout keyup', e => rangeInputFocusOutEvent(e));
+
+/**
+ * Actions to be done once one of the div is clicked. 
+ * It should hide the span, fade in the input, focus it, disable the self event and enable the input event.
+ */
+function rangeBoxClickEvent(rangeBox) {
+    $(rangeBox).children('span').hide();
+    $(rangeBox).children('input').fadeIn(300).focus();
+    $(rangeBox).removeClass('clickable').off('click');
+    $(rangeBox).children('input').on('focusout keyup', e => rangeInputFocusOutEvent(e));
+}
+
+function rangeInputFocusOutEvent(e) {
+    if (e.type == 'keyup' && e.keyCode != 13) {
+        return;
+    }
+    const parentDiv = $(e.target).parent();
+    $(parentDiv).children('input').hide();
     $(parentDiv).children('span').fadeIn(300);
-});
+    $(parentDiv).addClass('clickable').click(e => rangeBoxClickEvent(e.target));
+    $(e.target).off('focusout keyup');
+}
