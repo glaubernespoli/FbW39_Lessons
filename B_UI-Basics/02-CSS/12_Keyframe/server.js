d65579152aa89e7deb1b5a68178c32a1b7bcd340
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2020-09-08 14:37:21 +0200

    refactoring our repo --September 08 14:37

---
+++ B_UI-Basics/02-CSS/12_Keyframe/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const liveReload = require('livereload');
+const connectLiveReload = require('connect-livereload');
+
+//init our both servers
+const server = express();
+const liveReloadServer = liveReload.createServer();
+
+//use the middlewares
+server.use(connectLiveReload());
+server.use(express.static('public'));
+
+//watch the changes in our static folder
+liveReloadServer.watch('public');
+
+
+const port = 4531;
+server.listen(port, function () {
+    console.log(`Server is listening to the port: ${port}`);
+});
